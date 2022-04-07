c431ae639a1353254d7921bf8180cfe540a3be0e
Twirl <88945118+Twiirl@users.noreply.github.com>
2022-04-07 10:18:59 +0200

    Add files via upload

--- Jeu de tir/main.py
+++ Jeu de tir/main.py
@@ -4,17 +4,18 @@ import math
 from PIL import Image ,ImageTk
 from random import randint
 
-global all_cible
-all_cible=[]
-
 def create_window() :
     root = Tk()
     root.title("Jeu de tir")
+    root.geometry("1500x922")
+    root.minsize(1500,922)
+    root.iconbitmap("pointeur_0.ico")
+    root.config(background='#424242')
     return root
 
 def create_widgets():
-    graphic_zone = Canvas(root, width=1500, height=922 , bg = color)
-    graphic_zone.grid()
+    graphic_zone = Canvas(root, width=1499, height=921 , bg = color,highlightthickness=0)
+    graphic_zone.pack()
     return graphic_zone
 
 def create_background():
@@ -31,8 +32,7 @@ def create_cible():
     global cible
     global cible_
     cible=ImageTk.PhotoImage(Image.open("cible_0.png"),master=root)
-    cible_=graphic_zone.create_image(randint(40,1460),randint(80,620),anchor='center',image=cible)
-
+    cible_=graphic_zone.create_image(randint(60,1440),randint(80,620),anchor='center',image=cible)
     return cible
 
 # def cordo(event):
@@ -43,23 +43,39 @@ def create_cible():
 def tir(event):
     coordo=[]
     x,y=event.x,event.y
+    global stop
     global cible_
     global cible
     global vie
     global point
-    cordo=(graphic_zone.coords(cible_))
-    if cordo[0]-x<=40 and cordo[0]-x>=-40  and cordo[1]-y<=40 and cordo[1]-y>=-40 :
+    if stop:
+        cordo=(graphic_zone.coords(cible_))
+        if cordo[0]-x<=40 and cordo[0]-x>=-40  and cordo[1]-y<=40 and cordo[1]-y>=-40 :
+            graphic_zone.delete(cible)
+            point+=100
+            cible=create_cible()
+        else:
+            vie-=1
+            aff_vie['text'] = f"Vie : {vie}"
+        test()
+    else: 
+        pass
+        
+def test():
+    global cible
+    global vie
+    global stop
+    if vie ==0:
+        fin = Label(root, text="PERDU",font=("Courrier",50), fg="red",bg='#181818')
+        fin.place(x=650,y=400) 
         graphic_zone.delete(cible)
-        point+=100
-        cible=create_cible()
-    else:
-        vie-=1
-        if vie ==0:
-            print("finit")
+        stop=False
 
 color = "#282C34"
 global vie
 global point
+global stop
+stop = True
 timer=0
 point=0
 vie=5
@@ -72,6 +88,11 @@ root.config(cursor="tcross")
 # graphic_zone.bind("<Motion>",cordo)
 graphic_zone.bind("<ButtonPress-1>",tir)
 timer+=1
+
+aff_vie = Label(root, text=f"Vie : {vie}",font=("Courrier",18), fg="red",bg='#181818')
+aff_vie.place(x=1400,y=842)
+
+
 root.mainloop()
 
 
